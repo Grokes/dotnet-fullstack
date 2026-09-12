6bb31ffad9711ee65c60771e315905065d3b3467
grokes <default1one@gmail.com>
2026-09-12 19:21:51 +0300

    Исправлены опечатки

--- Backend/DirectoryService/src/DirectoryService.Application/Departments/IDepartmentsRepository.cs
+++ Backend/DirectoryService/src/DirectoryService.Application/Departments/IDepartmentsRepository.cs
@@ -2,7 +2,7 @@ using DirectoryService.Domain.Entities;
 
 namespace DirectoryService.Application.Departments;
 
-public interface IDepartmentsRepostitory
+public interface IDepartmentsRepository
 {
     public Task<Guid> AddAsync(Department department, CancellationToken cancellationToken);
     public Task<Department?> GetByIdAsync(Guid departmetnId, CancellationToken cancellationToken);
